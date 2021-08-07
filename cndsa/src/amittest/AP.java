a2b2c83df96c37fa3a27184c95545898b0812fb6
akadkol05 <akadkol05@gmail.com>
2021-08-07 22:08:18 +0530

    arithmetic progression

---
+++ cndsa/src/amittest/AP.java
@@ -0,0 +1,39 @@
+package amittest;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		System.out.println("n= ");
+		int n=s.nextInt();
+		
+		System.out.println("enter series= ");
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=s.nextInt();
+		}
+		System.out.println(arithmathic(arr,n));
+	}
+		
+		
+   public static boolean arithmathic(int arr[],int n) {
+	   Arrays.sort(arr);
+	   int  d=arr[1]-arr[0];
+		for(int i=2;i<n;i++) {
+			
+			if((arr[i]-arr[i-1])!=d) {
+			 return false;
+			}
+			
+		}
+		return true;
+		
+
+
+	}
+
+}
