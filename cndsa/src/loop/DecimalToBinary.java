329cd74f4676a90b44285e495dd058f7163788d6
akadkol05 <akadkol05@gmail.com>
2021-08-08 22:02:09 +0530

    Decimal to binary

---
+++ cndsa/src/loop/DecimalToBinary.java
@@ -0,0 +1,21 @@
+package loop;
+
+import java.util.Scanner;
+
+public class DecimalToBinary {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int n=sc.nextInt();
+		int t=n;
+		String s=" ";
+		while(t>0) {
+			int r=t%2;
+			t=t/2;
+			s=r+s;
+		}
+		System.out.println(s);
+	}
+
+}
