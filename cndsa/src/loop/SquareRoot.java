14ec9f1703f313cb9e22d187d8ce318483fa048d
akadkol05 <akadkol05@gmail.com>
2021-08-08 22:27:51 +0530

    Square root of a number

---
+++ cndsa/src/loop/SquareRoot.java
@@ -0,0 +1,17 @@
+package loop;
+
+import java.util.Scanner;
+
+public class SquareRoot {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int n=sc.nextInt();
+		int s=(int) Math.sqrt(n);
+		System.out.println(s);
+		
+
+	}
+
+}
