744ee0fe01b03607602111075037e7823ca56660
akadkol05 <akadkol05@gmail.com>
2021-08-11 20:52:40 +0530

    Pattern triangle  2

---
+++ cndsa/src/patterns/PatternTraingle2.java
@@ -0,0 +1,22 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class PatternTraingle2 {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int i=1;
+		while(i<=n) {
+			int j=1;
+			int start=i;
+			while(j<=i) {
+				System.out.print(start);
+				start++;
+				j++;
+			}
+			System.out.println();
+			i++;
+		}#
+	}
+}
