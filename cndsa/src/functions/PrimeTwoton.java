a36f39a2c0516c78afe4f654c1ca137d6490969b
akadkol05 <akadkol05@gmail.com>
2021-08-12 21:11:31 +0530

    print prime number from 2 to n using two functions

--- cndsa/src/functions/PrimeTwoton.java
+++ cndsa/src/functions/PrimeTwoton.java
@@ -1,7 +1,9 @@
 package functions;
 
 public class PrimeTwoton {
-	public static boolean isPrime(int n) {
+
+	
+public static boolean isPrime(int n) {
 		
 		for(int i=2;i<n;i++) {
 			if(n%i==0) {
@@ -15,13 +17,14 @@ public class PrimeTwoton {
 		for(int i=2;i<=n;i++) {
 			boolean isPrimenum=isPrime(i);
 			if(isPrimenum) {
+			
 				System.out.println(i);
 			}
 		}
 	}
 
 	public static void main(String[] args) {
-	  printprimes(100);
+	 printprimes(100);
 
 	}
 
