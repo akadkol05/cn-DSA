33b330be607dde8a1628d28018597bda83fe06d9
akadkol05 <akadkol05@gmail.com>
2021-08-14 22:15:28 +0530

    pattern triangle for numbers

---
+++ cndsa/src/patterns/PatternTriangles3.java
@@ -0,0 +1,25 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class PatternTriangles3 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int i=1;
+		int start=1;
+		while(i<=n) {
+			int j=1;
+			while(j<=i) {
+				System.out.print(start);
+				start++;
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+
+	}
+
+}
